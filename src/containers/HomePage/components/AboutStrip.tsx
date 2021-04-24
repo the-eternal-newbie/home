1553427784e92a589f4a4673d2ed735836d39fed
Carlos VP <ca.varaperez@gmail.com>
2021-04-24 14:33:31 -0500

    C-1: About strip v1

--- src/containers/HomePage/components/AboutStrip.tsx
+++ src/containers/HomePage/components/AboutStrip.tsx
@@ -1,22 +1,93 @@
 import React from 'react';
+import Button from '../../../components/Button/Button';
 import Portrait from '../../../components/Portrait/Portrait';
 import Text from '../../../components/Text/Text';
-import { AboutSection } from '../HomePage.styled';
+import { colors } from '../../../utils/styles/_colors';
+import { AboutBox, AboutContainer, AboutSection } from '../HomePage.styled';
 
 const AboutStrip: React.FC = () => {
     return (
         <AboutSection>
-            <Portrait size={'large'} variation={'circle'} filter={'greyscale'} url={''} />
-            <div>
-                <Text
-                    id={'Generic.Greetings'}
-                    style={{
-                        fontSize: '100px',
-                        fontWeight: 600,
-                        letterSpacing: '0.8px',
-                    }}
-                />
-            </div>
+            <AboutContainer>
+                <Portrait size={'large'} variation={'circle'} filter={'greyscale'} url={''} />
+                <AboutBox>
+                    <Text
+                        id={'Generic.Greetings'}
+                        style={{
+                            fontSize: '100px',
+                            fontWeight: 600,
+                            letterSpacing: '0.8px',
+                            marginBottom: '45px',
+                        }}
+                    />
+                    <Text
+                        id={'About.Title'}
+                        style={{
+                            fontSize: '22px',
+                            fontWeight: 600,
+                            letterSpacing: '0.8px',
+                        }}
+                    />
+                    <Text
+                        id={'About.Content.Short'}
+                        style={{
+                            fontSize: '16px',
+                            fontWeight: 300,
+                            fontFamily: "'Montserrat', sans-serif",
+                            letterSpacing: '0.8px',
+                            minWidth: '200px',
+                            maxWidth: '500px',
+                        }}
+                    />
+                    <div style={{ display: 'flex', width: '95%', justifyContent: 'space-between', marginTop: '20px' }}>
+                        <Button
+                            navigation
+                            to={'/resume'}
+                            text={'Generic.Resume'}
+                            type={'circle'}
+                            style={{
+                                width: '140px',
+                                height: '140px',
+                                background: colors.red1,
+                                color: colors.white1,
+                                fontSize: '16px',
+                                fontWeight: 600,
+                                textAlign: 'center',
+                            }}
+                        />
+                        <Button
+                            navigation
+                            to={'/resume'}
+                            text={'Generic.Projects'}
+                            type={'circle'}
+                            style={{
+                                width: '140px',
+                                height: '140px',
+                                background: colors.red2,
+                                color: colors.white1,
+                                fontSize: '16px',
+                                fontWeight: 600,
+                                textAlign: 'center',
+                            }}
+                        />
+                        <Button
+                            navigation
+                            to={'/resume'}
+                            text={'Generic.Contact'}
+                            type={'circle'}
+                            style={{
+                                width: '140px',
+                                height: '140px',
+                                backgroundColor: colors.red3,
+                                color: colors.white1,
+                                fontSize: '16px',
+                                fontWeight: 600,
+                                textAlign: 'center',
+                            }}
+                        />
+                    </div>
+                </AboutBox>
+            </AboutContainer>
         </AboutSection>
     );
 };
