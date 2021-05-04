8cf44121a12e025fe1946e09b5c1beb594fbb936
CarlosVP <ca.varaperez@gmail.com>
2021-05-03 21:20:01 -0500

    C1: Sticky menu bar v1

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -2,11 +2,10 @@
 import React from 'react';
 import { ILogo } from './Logo.types';
 import { LogoContainer, Square, Title } from './Logo.styled';
-import Text from '../../components/Text/Text';
 import { colors } from '../../utils/styles/_colors';
 
 const Logo: React.FC<ILogo> = (props: ILogo) => {
-    const { size } = props;
+    const { size, type } = props;
     const { width, height } =
         size === 'large'
             ? { width: '120px', height: '104px' }
@@ -14,20 +13,34 @@ const Logo: React.FC<ILogo> = (props: ILogo) => {
             ? { width: '62px', height: '58px' }
             : { width: '30px', height: '26px' };
     return (
-        <LogoContainer>
-            <Square style={{ width: width, height: height, zIndex: 1000 }}>
-                <Title>the eternal newbie</Title>
-            </Square>
-            <Square
-                style={{
-                    width: width,
-                    height: height,
-                    color: colors.yellow1,
-                    position: 'absolute',
-                    left: '16px',
-                    top: '18px',
-                }}
-            />
+        <LogoContainer
+            style={
+                type === 'full'
+                    ? { top: '10px', left: '60px' }
+                    : { position: 'initial', minWidth: '12%', padding: '0px 10px 10px 0px' }
+            }
+        >
+            {type === 'full' ? (
+                <>
+                    <Square style={{ width: width, height: height, zIndex: 1000 }}>
+                        <Title>the eternal newbie</Title>
+                    </Square>
+                    <Square
+                        style={{
+                            width: width,
+                            height: height,
+                            color: colors.yellow1,
+                            position: 'absolute',
+                            left: '16px',
+                            top: '18px',
+                        }}
+                    />
+                </>
+            ) : (
+                <Title style={{ color: colors.totalwhite, fontSize: '20px', marginLeft: '20px' }}>
+                    the eternal newbie
+                </Title>
+            )}
         </LogoContainer>
     );
 };
